5f2120a2d9b5f4d8dd7bb48d96802ababa77cfa4
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-01 16:59:52 +0800

    move routes to (site) to separate global style

---
+++ src/routes/(site)/+layout.svelte
@@ -0,0 +1,31 @@
+<script>
+	let { children } = $props();
+</script>
+
+<nav>
+	<img src="/dino.svg" alt="Stupid Dino Studios" width="64" height="64">
+	<a href="/">home</a>
+	<a href="/about">about</a>
+</nav>
+
+{@render children()}
+
+<style>
+	nav {
+		display: flex;
+		align-items: center;
+		gap: 1rem;
+		border-bottom: 1px solid black;
+
+		position: absolute;
+		width: 100vw;
+		left: 0;
+		top: 0;
+
+		height: 64px;
+	}
+
+	:global(body) {
+		margin-top: 72px;
+	}
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,36 +1,12 @@
-<script>
+<script lang='ts'>
+    import '$lib/main.css';
 	let { children } = $props();
 </script>
-
-<nav>
-	<img src="/dino.svg" alt="Stupid Dino Studios" width="64" height="64">
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
 
 {@render children()}
 
 <svelte:head>
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-	<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Oswald:wght@200..700&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </svelte:head>
-
-<style>
-	nav {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		border-bottom: 1px solid black;
-	}
-
-	:global(body) {
-		font-family: "Roboto", sans-serif;
-		font-optical-sizing: auto;
-		font-style: normal;
-		font-variation-settings:
-		"wdth" 100;
-
-		margin-top: 0;
-	}
-</style>
