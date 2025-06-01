3e12630eede22fe311800687da276e8541be9fc3
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-01 16:59:55 +0800

    Create +error.svelte

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { page } from '$app/state';
+</script>
+
+<div class='central'>
+    <h1>{page.status}</h1>
+    <h2>{page.error!.message}</h2>
+</div>
+
+<style>
+    .central {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: 100%;
+        height: 100%;
+        text-align: center; 
+    }
+</style>
