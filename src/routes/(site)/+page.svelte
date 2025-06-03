f41b3addcaa41c5d89800e170a2d85d8bdb1bcd7
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-03 16:18:36 +0800

    lipsum

--- src/routes/(site)/+page.svelte
+++ src/routes/(site)/+page.svelte
@@ -1,2 +1,18 @@
 <h1>Welcome to Stupid Dino Studios</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus vestibulum velit in felis semper, eget aliquam risus tristique. Nam neque augue, cursus non cursus nec, feugiat at tortor. Mauris elit ligula, tincidunt sit amet velit at, dapibus aliquam mi. Mauris elit ligula, condimentum ut orci vitae, ultricies hendrerit lacus. Nulla vestibulum lacus et malesuada luctus. Suspendisse id ipsum est. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Aliquam quam risus, interdum ac mollis vitae, sollicitudin vel nulla. Suspendisse et eros ante. Suspendisse ultrices id leo in pellentesque. Pellentesque in ornare neque. Integer ac nisi vitae ipsum laoreet rhoncus. Integer nisl nibh, eleifend sed egestas vel, maximus pretium metus. Praesent non fermentum felis. Fusce posuere orci eget tortor placerat tincidunt.</p>
+
+<p>Integer quis dignissim erat. Pellentesque fringilla leo eu ligula placerat, in scelerisque urna condimentum. Fusce elit magna, vulputate porttitor nisi at, malesuada luctus elit. Nam vel eros tempus, condimentum massa a, viverra odio. Nullam porta suscipit urna porta laoreet. Donec sed imperdiet mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+
+<p>Proin sed interdum dui. Quisque eget purus lectus. Donec id felis aliquet magna lacinia varius sit amet in justo. Mauris a aliquet leo. In mauris est, fringilla at tortor eu, iaculis rhoncus felis. Proin ut sagittis mi. Aliquam eget elementum arcu, et dapibus eros. Nam auctor nibh facilisis lorem pellentesque, quis porta turpis pretium. Sed dignissim, dui nec congue luctus, tellus ante ornare mauris, vitae eleifend magna odio at tortor. Aenean vel ultrices ligula. Nam vehicula mauris dui, nec tempor ipsum ornare eget. Aliquam quis enim est. Pellentesque laoreet ipsum eget nibh pulvinar, a dapibus nunc egestas.</p>
+
+<p>Quisque pretium ullamcorper eleifend. Nunc faucibus nibh eget pulvinar commodo. Sed id consectetur metus. Sed porta libero neque. Vestibulum consequat eros feugiat dictum iaculis. Etiam mollis, nibh ut rutrum volutpat, nulla magna ultricies tellus, vel feugiat tellus magna a metus. Proin erat dui, dictum ut ipsum vel, finibus viverra justo. Vestibulum sit amet sem rhoncus libero congue molestie. Suspendisse fermentum, ante eget egestas eleifend, felis massa tincidunt orci, ac fermentum diam felis sed ex.</p>
+
+<p>Curabitur auctor sollicitudin ex id molestie. Suspendisse vitae leo ut velit porttitor facilisis. Suspendisse massa velit, placerat at interdum et, ultricies vel dolor. Sed tempor elementum erat, eu molestie lacus egestas et. In mauris ex, egestas eu nulla vitae, euismod facilisis augue. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Mauris at faucibus eros, ac suscipit arcu. Phasellus semper neque vel eros varius, in egestas odio lobortis. Etiam luctus egestas maximus. Vestibulum tellus dolor, aliquet ut sagittis ut, placerat porta lectus. Morbi commodo suscipit feugiat. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+
+<style>
+    p {
+        font-size: 1.2em;
+    }
+</style>
