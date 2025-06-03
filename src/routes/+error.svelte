fb96b5a0820203f258fa6983aec968a76cb6ed91
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-03 17:40:16 +0800

    center error info vertically

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,18 +2,30 @@
     import { page } from '$app/state';
 </script>
 
-<div class='central'>
-    <h1>{page.status}</h1>
-    <h2>{page.error!.message}</h2>
+<div class="max">
+    <div class='central'>
+        <h1>{page.status}</h1>
+        <h2>{page.error!.message}</h2>
+    </div>
 </div>
 
 <style>
+    .max {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        width: 100vw;
+        height: 100vh;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+
     .central {
         display: flex;
         flex-direction: column;
         justify-content: center;
-        width: 100%;
-        height: 100%;
         text-align: center; 
     }
 </style>
