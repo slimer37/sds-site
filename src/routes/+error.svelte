65642c538d8fe82ccb7b095de6c704a20d83b430
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-03 17:50:40 +0800

    gray dino in bg of error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,7 @@
     import { page } from '$app/state';
 </script>
 
+<div class="max bg-dino"></div>
 <div class="max">
     <div class='central'>
         <h1>{page.status}</h1>
@@ -10,6 +11,15 @@
 </div>
 
 <style>
+    .bg-dino {
+        background-image: url('dino.svg');
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: 20em;
+
+        filter: contrast(0);
+    }
+
     .max {
         display: flex;
         align-items: center;
@@ -27,5 +37,15 @@
         flex-direction: column;
         justify-content: center;
         text-align: center; 
+
+        padding-bottom: 40px;
+    }
+    
+    h1 {
+        font-size: 5em;
+    }
+
+    h2 {
+        font-size: 1.5em;
     }
 </style>
