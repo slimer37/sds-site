c84dee262b49120c28cd776864aef1d9bf780cf8
Alfred (Mac) <59073967+slimer37@users.noreply.github.com>
2025-06-03 16:18:40 +0800

    shrinking header

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -1,31 +1,77 @@
-<script>
+<script lang='ts'>
 	let { children } = $props();
+
+	let header: Element;
+
+	function shrinkWhenScrolled() {
+		const scrollThreshold = 50;
+		
+		if (document.body.scrollTop > scrollThreshold || document.documentElement.scrollTop > scrollThreshold) {
+			header.classList.add("shrunk-header");
+		} else {
+			header.classList.remove("shrunk-header");
+		}
+	}
 </script>
 
-<nav>
-	<img src="/dino.svg" alt="Stupid Dino Studios" width="64" height="64">
-	<a href="/">home</a>
-	<a href="/about">about</a>
-</nav>
+<svelte:window on:scroll={shrinkWhenScrolled} />
 
-{@render children()}
+<div class="max-w-screen max-h-screen p-0 m-0">
+
+	<nav class="flex justify-between items-center p-3" bind:this={header}>
+		<a href="/"><img src="/dino.svg" alt="Stupid Dino Studios" width=64 height=64></a>
+		<div class='btn-container'>
+			<a href="/about">about</a>
+		</div>
+	</nav>
+
+	<div class="content-container">
+		{@render children()}
+	</div>
+	
+</div>
 
 <style>
-	nav {
-		display: flex;
-		align-items: center;
-		gap: 1rem;
-		border-bottom: 1px solid black;
-
-		position: absolute;
-		width: 100vw;
-		left: 0;
-		top: 0;
-
-		height: 64px;
+	:root {
+		--nav-height: 80px;
+		--shrunk-nav-height: 60px;
 	}
 
-	:global(body) {
-		margin-top: 72px;
+	nav {
+		border-bottom: 1px solid black;
+		
+		position: fixed;
+		width: 100%;
+		top: 0;
+		height: var(--nav-height);
+		
+		transition: 0.2s cubic-bezier(0.075, 0.82, 0.165, 1);
+		
+		z-index: 1000;
+
+		background-color: white;
+
+		font-size: 1em;
+	}
+
+	:global(.shrunk-header) {
+		height: var(--shrunk-nav-height) !important;
+		font-size: 0.75em;
+	}
+
+	.btn-container {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		gap: 1rem;
+
+		padding: 5px;
+
+		transition-duration: 1s;
+	}
+
+	.content-container {
+		margin-top: var(--nav-height);
+		padding: 10px;
 	}
 </style>
